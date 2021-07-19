4590041e553c5a2d760ce5bc1e0bac03906615cd
franciscoguinarte <francisco.guinarte@outlook.com.br>
2021-07-19 12:09:07 -0300

    add: video table with constraints

--- src/main/java/br/com/aluraflix/controller/AluraflixController.java
+++ src/main/java/br/com/aluraflix/controller/AluraflixController.java
@@ -1,5 +1,13 @@
 package br.com.aluraflix.controller;
 
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/aluraflix")
 public class AluraflixController {
 
+	
+	 
+	
 }
